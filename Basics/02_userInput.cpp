44a951c320de744ff197d5e5d8101af3df824d36
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2025-02-18 18:03:30 +0000

    commit

---
+++ Basics/02_userInput.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    char alpha;
+    float flu1;
+    double dob1;
+    cout<<"Enter the marks: "<<endl;
+    cin>>marks;
+
+    cout<<"Enter the Alpha: "<<endl;
+    cin>>alpha;
+
+    cout<<"Enter the flu1: "<<endl;
+    cin>>flu1;
+
+    cout<<"Enter the dob1: "<<endl;
+    cin>>dob1;
+
+    cout<<"The marks is: "<<marks<<endl;
+    cout<<"The alpha is: "<<alpha<<endl;
+    cout<<"The flu1 is: "<<flu1<<endl;
+    cout<<"The dob1 is: "<<dob1<<endl;
+
+}
