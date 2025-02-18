bd32633e42ee2bb79e05046c0529cb5f390471d2
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2025-02-18 19:00:43 +0000

    commit

--- Basics/operators.cpp
+++ Basics/operators.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // Unary Operators
-    cout<<"Unary Operators"<<endl;
+    cout << "Unary Operators" << endl;
 
     int a = 5;
     cout << "Before increment ++ :" << a << endl;
@@ -18,7 +18,7 @@ int main()
     cout << "After increment -- :" << a << endl;
 
     // Arithmetic Operators
-    cout<<"Arithmetic Operators"<<endl;
+    cout << "Arithmetic Operators" << endl;
 
     int num1 = 15;
     int num2 = 3;
@@ -28,14 +28,50 @@ int main()
     cout << (num1 / num2) << endl;
     cout << (num1 % num2) << endl;
 
-    //Relational Operators
-    cout<<"Relational Operators"<<endl;
+    // Relational Operators
+    cout << "Relational Operators" << endl;
     cout << (num1 > num2) << endl;
     cout << (num1 < num2) << endl;
     cout << (num1 >= num2) << endl;
     cout << (num1 <= num2) << endl;
     cout << (num1 == num2) << endl;
     cout << (num1 != num2) << endl;
+
+    // Assignment Operators
+    cout << "Assignment Operators" << endl;
+
+    int x = 10;
+    x += 5;
+    cout << x << endl;
+    x -= 5;
+    cout << x << endl;
+    x *= 5;
+    cout << x << endl;
+    x /= 5;
+    cout << x << endl;
+    x %= 5;
+    cout << x << endl;
+
+    // Logical Operators
+    cout << "Logical Operators" << endl;
+    int p = 5;
+    int q = 6;
+
+    cout << (p && q) << endl;
+    cout << (p || q) << endl;
+    cout << (!p) << endl;
+
+    //Bitwise Operators
+    cout << "Bitwise Operators" << endl;
+    int m = 5,n = 7;
+    cout << (m & n) << endl;
+    cout << (m | n) << endl;
+    cout << (m ^ n) << endl;
+    cout << (~m) << endl;
+    cout << (m << 1) << endl;
+    cout << (m >> 1) << endl;
+
+
 
     return 0;
 }
