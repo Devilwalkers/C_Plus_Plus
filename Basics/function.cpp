f0151f5248ad152d07b8e5d09ba34a1f029c1fd9
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2025-02-19 15:47:37 +0000

    commit

--- Basics/function.cpp
+++ Basics/function.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+void counting(){
+    for (int i = 1; i <= 5; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+int sum(int a, int b){
+    int sum = 0;
+    sum = a + b;
+    return sum;
+}
+float float1(float a, float b){
+    float sum = 0.0;
+    sum = a + b;
+    return sum;
+}
 int main(){
-    
+
+    counting();
+
+    int answer = sum(5, 6);
+    cout << answer << endl;
+
+    float answer1 = float1(5.0, 8.5);
+    cout << answer1 << endl;
 
     return 0;
 }
