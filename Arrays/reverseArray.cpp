5163ddb0655c04eeea686866219ca95babb5f58d
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2025-03-10 18:25:38 +0000

    commit

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void reverseArray(int arr[],int n){
@@ -15,9 +15,30 @@ void reverseArray(int arr[],int n){
 	}
 }
 
-int main(){
-	int n = 7, arr[]={2,5,3,4,6,7,8};
+void reverseArrayBitwise(int arr[], int n)
+{
+	int left = 0;
+	int right = n - 1;
+	while (left <= right)
+	{
+		arr[left] = arr[left] ^ arr[right];
+		arr[right] = arr[left] ^ arr[right];
+		arr[left] = arr[left] ^ arr[right];
+		left++;
+		right--;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int main()
+{
+	int n = 7, arr[] = {2, 5, 3, 4, 6, 7, 8};
 
 	reverseArray(arr, n);
+	cout << endl;
+	reverseArrayBitwise(arr, n);
 	return 0;
 }
--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,1 +1,13 @@
-INT_MIN
+void reverseArray(int arr[],int n){
+// 	int left = 0;
+// 	int right = n - 1;
+
+// 	while(left<=right){
+// 		swap(arr[left], arr[right]);
+// 		left++;
+// 		right--;
+// 	}
+// 	for (int i = 0; i < n;i++){
+// 		cout << arr[i] << " ";
+// 	}
+// }
