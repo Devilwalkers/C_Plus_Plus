8cb5540f21a27fc15efc69241c8f900093d6ebc0
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2025-03-18 18:26:46 +0000

    commit

--- STL/map.cpp
+++ STL/map.cpp
@@ -5,21 +5,65 @@
 using namespace std;
 int main()
 {
-    unordered_map<string,string> um;
+    map<string, string> um;
+    unordered_map<string, string> um1;
 
-    //Insertion methods
-    um["br"]= "Brazil";// method 1
+    // Insertion methods
+    um["br"] = "Brazil"; // method 1
 
     um.insert(make_pair("in", "India")); // method 2
 
-    pair<string, string> p;// method 3
-    p.first ="en";
-    p.second ="England";
+    pair<string, string> p; // method 3
+    p.first = "en";
+    p.second = "England";
     um.insert(p);
-    
-    cout<<um.at("en")<<endl;
 
-    
+    // cout << um.at("en") << endl;
+
+    um.at("en") = "England 2";
+    // cout << um.at("en") << endl;
+
+    um.at("en") = "England 3";
+    // cout << um.at("en") << endl;
+    // um.clear();
+    map<string, string>::iterator it = um.begin();
+
+    while (it != um.end())
+    {
+        pair<string, string> p = *it;
+        cout << p.first << ", " << p.second << endl;
+        it++;
+    }
+
+    um1["1"] = "Intro";
+    um1["2"] = "C++";
+    um1["3"] = "DSA";
+
+    // um.swap(um1);
+    // unordered_map<string, string>::iterator it = um.begin();
+
+    // while (it != um.end())
+    // {
+    //     pair<string, string> p = *it;
+    //     cout << p.first << ", " << p.second << endl;
+    //     it++;
+    // }
+    um1.erase(um1.begin(),um1.end());
+    cout<<um1.size()<<endl;
+
+    if(um.find("in")!=um.end()){
+        cout<<"Key found!"<<endl;
+    }
+    else{
+        cout<<"Key not found!"<<endl;
+
+    }
+    if(um.count("in") == 0){
+        cout<<"Key not found!"<<endl;
+    }
+    if(um.count("in") == 1){
+        cout<<"Key not found!"<<endl;
+    }
 
     return 0;
 }
